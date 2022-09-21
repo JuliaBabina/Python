960c6ecea32c948502bbe46867b6be311bbd97f1
Julia Babina <u.babina55@gmail.com>
2022-09-21 23:24:11 +0500

    add lec1

---
+++ Hello_Python/lec1.py
@@ -0,0 +1,57 @@
+# print ('hello world')
+
+from trace import Trace
+
+
+# value = None
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+
+# value = 12334
+# print(value)
+
+# print(type(a))
+# print(type(b))
+# print(type(value))
+
+# s = 'hello world'
+# print(s)
+# s = 'hello \nworld'
+# print(s)
+# print (a,b,s)
+# print(a, '-', b, '-', s)
+# print ('{} - {} - {}'.format(a,b,s))
+# print (f'{a} - {b} - {s}')
+# print ('{1} - {2} - {0}'.format(a,b,s))
+
+# f = True
+# print(f)
+
+# p = False
+# print(p)
+
+# list = []
+# print(list)
+
+# list = [1,2,3]
+# print(list)
+
+# list = ['1','2','3', 'hello']
+# print(list)
+
+print('Введите a: ');
+# a = int (input())
+a = float (input())
+print('Введите b: ');
+# b = int (input())
+b = float (input())
+
+print (a,b)
+print ('{} {}'.format(a,b))
+print (f'{a} {b}')
+print (a, '+' ,b, '=' , a+b)
+
+
+
