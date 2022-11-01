4dba72b2674f0084443db3d0ada6a5919cba97f9
Julia Babina <u.babina55@gmail.com>
2022-11-01 22:58:22 +0500

    add lec 9 bot

---
+++ Hello_Python/lec9.py
@@ -0,0 +1,86 @@
+# from progress.bar import Bar
+# import time
+
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(1)
+#     bar.next()
+# bar.finish()
+
+
+
+
+# import emoji
+
+# print(emoji.emojize('Python is :thumbs_up:'))
+# # Python is 👍
+# print(emoji.emojize('Python is :thumbsup:', language='alias'))
+# # Python is 👍
+# print(emoji.demojize('Python is 👍'))
+# # Python is :thumbs_up:
+# print(emoji.emojize("Python is fun :red_heart:"))
+# # Python is fun ❤
+# print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
+# # Python is fun ❤️ #red heart, not black heart
+# print(emoji.is_emoji("👍"))
+# # True
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # Fixing random state for reproducibility
+# np.random.seed(19680801)
+
+
+# plt.rcdefaults()
+# fig, ax = plt.subplots()
+
+# # Example data
+# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+# y_pos = np.arange(len(people))
+# performance = 3 + 10 * np.random.rand(len(people))
+# error = np.random.rand(len(people))
+
+# ax.barh(y_pos, performance, xerr=error, align='center')
+# ax.set_yticks(y_pos, labels=people)
+# ax.invert_yaxis()  # labels read top-to-bottom
+# ax.set_xlabel('Performance')
+# ax.set_title('How fast do you want to go today?')
+
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# list = [1,2,3,2,7]
+# plt.plot(list)
+
+# plt.show()
+
+
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+# async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5580170538:AAHkEaFTjlNJ-qZ4WSs4TVAPVH3r2phEknA").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+
+print("server start")
+app.run_polling()
